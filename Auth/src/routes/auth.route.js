e2edc151b961cb554f4250c8a4eebd624c318292
Fajar <133589667+FajarMaulana9971@users.noreply.github.com>
2024-08-10 20:05:35 +0700

    Feat(Auth): add another feature route

--- Auth/src/routes/auth.route.js
+++ Auth/src/routes/auth.route.js
@@ -1,14 +1,26 @@
 import express from "express";
 import {
+  forgotPassword,
+  getAll,
   login,
+  logout,
+  refreshAccessToken,
   register,
   verifyAccount,
+  verifyToken,
 } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
 router.post(process.env.BASE_ROUTER + "auth/register", register);
-router.post(process.env.BASE_ROUTER + "auth/verify", verifyAccount);
+router.post(process.env.BASE_ROUTER + "auth/account-verify", verifyAccount);
 router.post(process.env.BASE_ROUTER + "auth/login", login);
+router.post(
+  process.env.BASE_ROUTER + "auth/refresh-access-token",
+  refreshAccessToken,
+);
+router.post(process.env.BASE_ROUTER + "auth/logout/:accountId", logout);
+router.post(process.env.BASE_ROUTER + "auth/forgot-password", forgotPassword);
+router.get(process.env.BASE_ROUTER + "auth/get-all", verifyToken, getAll);
 
 export default router;
