aabc14907bc5f55d53ff74c6d241656a45d26f75
Fajar <133589667+FajarMaulana9971@users.noreply.github.com>
2024-08-12 14:25:49 +0700

    Fix(Auth): change auth route

--- Auth/src/routes/auth.route.js
+++ Auth/src/routes/auth.route.js
@@ -12,15 +12,15 @@ import {
 
 const router = express.Router();
 
-router.post(process.env.BASE_ROUTER + "auth/register", register);
-router.post(process.env.BASE_ROUTER + "auth/account-verify", verifyAccount);
-router.post(process.env.BASE_ROUTER + "auth/login", login);
+router.post(process.env.BASE_ROUTER + "register", register);
+router.post(process.env.BASE_ROUTER + "account-verify", verifyAccount);
+router.post(process.env.BASE_ROUTER + "login", login);
 router.post(
-  process.env.BASE_ROUTER + "auth/refresh-access-token",
+  process.env.BASE_ROUTER + "refresh-access-token",
   refreshAccessToken,
 );
-router.post(process.env.BASE_ROUTER + "auth/logout/:accountId", logout);
-router.post(process.env.BASE_ROUTER + "auth/forgot-password", forgotPassword);
-router.get(process.env.BASE_ROUTER + "auth/get-all", verifyToken, getAll);
+router.post(process.env.BASE_ROUTER + "logout", verifyToken, logout);
+router.post(process.env.BASE_ROUTER + "forgot-password", forgotPassword);
+router.get(process.env.BASE_ROUTER + "get-all", verifyToken, getAll);
 
 export default router;
