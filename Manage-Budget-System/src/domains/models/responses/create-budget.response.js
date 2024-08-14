e133543b9e66de8a753672fa807866164673a064
Fajar <133589667+FajarMaulana9971@users.noreply.github.com>
2024-08-14 16:26:10 +0700

    Feat(MBS): Add DTO

---
+++ Manage-Budget-System/src/domains/models/responses/create-budget.response.js
@@ -0,0 +1,21 @@
+class CreateBudgetResponse {
+    constructor(
+        budgetId,
+        title,
+        description,
+        total_balance,
+        incomeId,
+        outcomeId,
+        userId
+    ) {
+        this.budgetId = budgetId;
+        this.title = title;
+        this.description = description;
+        this.total_balance = total_balance;
+        this.incomeId = incomeId;
+        this.outcomeId = outcomeId;
+        this.userId = userId;
+    }
+}
+
+export default CreateBudgetResponse;
