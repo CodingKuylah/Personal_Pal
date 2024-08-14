067e7e06d96d5a1d353711d5aa51156a6952cd44
Fajar <133589667+FajarMaulana9971@users.noreply.github.com>
2024-08-14 16:12:16 +0700

    Feat(MBS): add budget controller function

--- Manage-Budget-System/src/controllers/budget.controller.js
+++ Manage-Budget-System/src/controllers/budget.controller.js
@@ -2,6 +2,13 @@ import Budget from "../domains/entities/budget.entity.js";
 import { handleFailedResponse } from "../utils/response-handling/failed-response/handle.failed-response.js";
 import { handleSuccessResponse } from "../utils/response-handling/success-response/handle.success-response.js";
 import { handleErrorResponse } from "../utils/response-handling/error-response/handle.error-response.js";
+import CreateBudgetRequest from "../domains/models/requests/create-budget.request.js";
+import Income from "../domains/entities/income.entity.js";
+import Outcome from "../domains/entities/outcome.entity.js";
+import BudgetUser from "../domains/entities/transactional-table.entity/budget-user.transactional.js";
+import IncomeUser from "../domains/entities/transactional-table.entity/income-user.transactional.js";
+import OutcomeUser from "../domains/entities/transactional-table.entity/outcome-user.transactional.js";
+import CreateBudgetResponse from "../domains/models/responses/create-budget.response.js";
 
 async function getBudgetById(req, res) {
   try {
@@ -29,4 +36,63 @@ async function getBudgetById(req, res) {
   }
 }
 
-export { getBudgetById };
+async function createBudget(req, res){
+  const {title, description, total_balance} = req.body;
+  const userId = req.accountId;
+  const createBudgetDTO = new CreateBudgetRequest(title, description, total_balance)
+  const validationErrors = createBudgetDTO.validate();
+
+  if(validationErrors.length > 0){
+  return handleFailedResponse(res, validationErrors, 500);
+  }
+
+  const finalTotalBalance = total_balance || 0.0;
+  try{
+    const newBudget = await Budget.create({
+      title: createBudgetDTO.title,
+      description: createBudgetDTO.description,
+      total_balance: finalTotalBalance,
+    })
+
+    const newIncome = await Income.create({
+      description: "FIRST COMMIT (Default by systems)",
+      amount: 0.0,
+      created_by: "Systems"
+    })
+
+    const newOutcome = await Outcome.create({
+      description: "FIRST COMMIT (Default by systems)",
+      amount: 0.0,
+      created_by: "Systems"
+    })
+
+    await BudgetUser.create({
+      budget_id : newBudget.id,
+      user_id: userId
+    });
+
+    await IncomeUser.create({
+      income_id: newIncome.id,
+      user_id: userId
+    });
+
+    await OutcomeUser.create({
+      outcome_id: newOutcome.id,
+      user_id: userId
+    })
+
+    const handlingResponse = new CreateBudgetResponse(
+        newBudget.id,
+        newBudget.title,
+        newBudget.description,
+        newIncome.id,
+        newOutcome.id,
+        userId
+    )
+    return handleSuccessResponse(res, handlingResponse, 200, "New Budget Successfully Created")
+  }catch (err){
+    return handleErrorResponse(res, err)
+  }
+}
+
+export { getBudgetById, createBudget };
